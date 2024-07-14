e2713b5f5be5e9f92e5dca77c0ef1f27a68f0a62
Bishesh Agarwal <bisheshagarwal1999@gmail.com>
2024-07-14 12:35:48 +0530

    feat (Tooltip): wrap a custom Hint component around Buttons and Items to show a ToolTip.

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -4,15 +4,23 @@ import { CreateOrganization } from "@clerk/nextjs";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { Hint } from "@/components/hint";
 
 export const NewButton = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-          <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
-            <Plus className="text-white" />
-          </button>
+          <Hint
+            label="Create organization"
+            side="right"
+            align="start"
+            sideOffset={15}
+          >
+            <button className="bg-white/25 h-full w-full rounded-md flex items-center justify-center opacity-60 hover:opacity-100 transition">
+              <Plus className="text-white" />
+            </button>
+          </Hint>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
